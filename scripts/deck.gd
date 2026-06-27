9f5c85c4682419ede9a507ecba67ef2abdb62b47
Leo-Lu-1031 <43920346+Leo-Lu-1031@users.noreply.github.com>
2026-06-27 02:33:36 +0100

    Deleted References

--- scripts/deck.gd
+++ scripts/deck.gd
@@ -4,8 +4,6 @@ class_name Deck
 const CARD_WIDTH = 100
 
 var center_screen_x
-var deck_array = []
-var hand_array = []
 
 # Set by parent
 var deck_position: Vector2
@@ -13,20 +11,21 @@ var deck_position: Vector2
 func set_deck_position(pos: Vector2) -> void:
 	deck_position = pos
 	position = pos
-	for i in range(deck_array.size()):
-		var card = deck_array[i]
+	for i in range(card_array.size()):
+		var card = card_array[i]
 		card.position = deck_position - 2 * min(i,10) * Vector2(0, 1)
 
 func shuffle() -> void:
-	deck_array.shuffle()
+	card_array.shuffle()
 	
 #func draw(card: Card) -> void:
 	#hand_reference.add_card_to_hand(card)
 	#remove_card_from_deck(card)
 
-func remove_card_from_deck(card: Card) -> void:
-	if card in deck_array:
-		deck_array.erase(card)
+func remove_card(card: Card) -> void:
+	if card in card_array:
+		card_array.erase(card)
+
 """Unfinished - add update deck position here once done"""
 
 
--- scripts/hand.gd
+++ scripts/hand.gd
@@ -3,32 +3,24 @@ class_name Hand
 
 const HAND_COUNT = 3
 
-const CARD_SCENE_PATH = "res://scenes/card.tscn"
-const CARDS_FOLDER_PATH = "res://assets/Cards_Folder/"
-
 @export var IS_AT_TOP = false
 @export var SHOW_FRONT = false
 @export var DECK_POS: Vector2
 
-var hand_array = []
 var center_screen_x
 var hand_y_pos
-
-@onready var CardManager = $"CardManager"
-@export var Deck: Deck
 
 # Called when the node enters the scene tree for the first time
 func _ready() -> void:
 	var screen_size = get_viewport().size
 	center_screen_x = screen_size.x / 2.0
 	hand_y_pos = screen_size.y / 2.0 + (screen_size.y / 2.0 - 75 - Global.CARD_HEIGHT/2) * (1 if IS_AT_TOP else -1)
-	Deck.set_deck_position(DECK_POS)
 
 #@Override
 func add_card(card: Card) -> void:
 	if card is Card:
-		if card not in hand_array:
-			hand_array.insert(0,card)
+		if card not in card_array:
+			card_array.insert(0,card)
 			update_hand_positions()
 			if !card.showing_front and SHOW_FRONT:
 				card.show_front()
@@ -38,15 +30,15 @@ func add_card(card: Card) -> void:
 			animate_card_to_position(card,card.starting_position)
 
 func update_hand_positions() -> void:
-	for i in range(hand_array.size()):
+	for i in range(card_array.size()):
 		var new_position = Vector2(calculate_card_position(i), hand_y_pos)
-		var card = hand_array[i]
-		print(self, hand_array)
+		var card = card_array[i]
+		print(self, card_array)
 		card.starting_position = new_position
 		animate_card_to_position(card, new_position)
 		
 func calculate_card_position(index: int) -> float:
-	var total_width = (hand_array.size() - 1)*Global.CARD_WIDTH
+	var total_width = (card_array.size() - 1)*Global.CARD_WIDTH
 	var x_position = center_screen_x + index*Global.CARD_WIDTH - total_width/2.0
 	return x_position
 	
@@ -56,6 +48,6 @@ func animate_card_to_position(card: Card, new_position: Vector2) -> void:
 
 #@Override
 func remove_card(card: Card) -> void:
-	if card in hand_array:
-		hand_array.erase(card)
+	if card in card_array:
+		card_array.erase(card)
 		update_hand_positions()
