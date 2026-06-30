54735646d6358388b526f2b7b0039a2b9225c0f1
Mimikyu2988 <125899084+Mimikyu2988@users.noreply.github.com>
2026-06-30 17:51:37 -0400

    a

--- scripts/card.gd
+++ scripts/card.gd
@@ -20,15 +20,13 @@ var memorized_z_index := 0
 
 var card_owner: CardCollection
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	get_parent().connect_card_signals(self)
 	show_front()
 	$"Glow".visible = false
 	$"Fog".visible = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+
 
 func _on_area_2d_mouse_entered() -> void:
 	emit_signal("hovered", self)
